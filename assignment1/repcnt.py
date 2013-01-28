ea38505292e8592bd35d6bdb2f7cd6b76a58d60b
Andres <andresf.mon@gmail.com>
2013-01-27 17:53:59 -0700

    initial

---
+++ assignment1/repcnt.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import rep
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+# A function to return the number of '[' in a given 'n' 'set of sets'
+def repcnt (n):
+    """
+    Return the number of open brackets '[' in the 'set of sets given by 'n'
+    """
+    output = str(rep.rep(n))
+    return output.count("[")
+
+
+if __name__ == '__main__': #block, include the test    
+    for i in range(8):
+        pp.pprint(("Number of [ in " + str(i) + " is: ", repcnt(i)))
+
+    help (repcnt)
